7d2173501346adbe4a54d7a68a81a89373108500
Wiramon Magird - 6809617407 <stang.wiramol@gmail.com>
2025-11-11 16:46:06 +0700

    Add files via upload

---
+++ swapmatrix.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+	
+	int A[4][5];
+	int a, b, c, d, temp, i, j, count = 0, max;
+	
+	for (i=0; i<4; i++){
+		for (j=0; j<5; j++){
+			scanf("%d", &A[i][j]);
+		}
+	}
+	
+	scanf("%d %d", &a, &b);
+	scanf("%d %d", &c, &d);
+	
+	temp = A[a][b];
+	A[a][b] = A[c][d];
+	A[c][d] = temp;
+	
+	for (i=0; i<4; i++){
+		max = [i][0];
+		for (j=0; j<5; j++){
+			if (A[i][j] > max){
+				max = A[i][j];
+			}
+		}
+		printf("%d ", max);
+	}
+	
+	return 0;
+}
